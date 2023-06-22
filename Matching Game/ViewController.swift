e87fadd7e95264d1ab09cf3391ed5b32ae9ca08b
Christos Papachristou <christospapachristous@gmail.com>
2023-06-22 18:44:14 +0300

    v 1.0 (card emoji is given by array)

--- Matching Game/ViewController.swift
+++ Matching Game/ViewController.swift
@@ -10,7 +10,31 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var flipCountLabel: UILabel!
-    
     @IBOutlet var cardButtons: [UIButton]!
+    
+    var emojiChoices = ["🎃","👻","🎃","👻"]
+    var flipCount = 0 {
+        didSet{
+            flipCountLabel.text = "Flips: \(flipCount)"
+        }
+    }
+    
+    @IBAction func touchCard(_ sender: UIButton) {
+        flipCount += 1
+        if let cardNumber = cardButtons.firstIndex(of: sender){
+            flipCard(withEmoji: emojiChoices[cardNumber], on: sender)
+        }else{
+            print("Card not in Buttons")
+        }
+    }
+    func flipCard(withEmoji emoji: String, on button: UIButton){
+        if button.currentTitle == emoji{
+            button.setTitle("", for: UIControl.State.normal)
+            button.backgroundColor = UIColor.yellow
+        }else{
+            button.setTitle(emoji, for: UIControl.State.normal)
+            button.backgroundColor = UIColor.white
+        }
+    }
 }
 
