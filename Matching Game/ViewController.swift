a13aee9b79b27d2f4039c47cfc3731db39b310f9
Christos Papachristou <44834955+chrispapachristou@users.noreply.github.com>
2023-06-22 14:32:51 +0300

    Add files via upload

---
+++ Matching Game/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Matching Game
+//
+//  Created by Christos Papachristou on 22/6/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var flipCountLabel: UILabel!
+    
+    @IBOutlet var cardButtons: [UIButton]!
+}
+
